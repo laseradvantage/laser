62cdb12e64383e59a0a4ff8b52dcf51d3f5f171c
andrewgadziksonos <andrewgadzik@gmail.com>
2017-10-09 08:08:55 -0400

    Drupal install

--- app-root/data/sites/default/files/js/js_pF2hY2q4ZXFBNLWXkJM725wESyWCKPKt33lQvtZYa7Q.js
+++
@@ -1,8 +0,0 @@
-(function($) { 
-  $(document).ready(function() {
-    linkhtml = $('#home-link').html();
-    $('#home-link').html('<i class="icon-home"></i>' + linkhtml);
-    linkhtml = $('#contact-link').html();
-    $('#contact-link').html('<i class="icon-envelope"></i>' + linkhtml)
-  });
-})(jQuery);;
